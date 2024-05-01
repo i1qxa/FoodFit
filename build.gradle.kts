dc3ef63d7d50493f451bf2dca144272f500f0b43
Иван <cvb@sdf>
2024-05-02 00:59:43 +0300

    Закончил обновление

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,11 @@
+buildscript {
+    dependencies {
+        classpath(libs.google.services)
+    }
+}
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
     id("com.google.devtools.ksp") version "1.9.21-1.0.15" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
 }
